db92a22bef5cb608d3ce35867388d47af3549cea
Manoj Paloi <paloimanoj@gmail.com>
2025-09-01 21:42:30 +0530

    Initial commit

--- drfcommerce/settings/base.py
+++ drfcommerce/settings/base.py
@@ -23,6 +23,7 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
+
 # -------------------------------------------------------------------
 # Installed apps
 # -------------------------------------------------------------------
