afab75c8e0c6c53e98a2ec5ff0424cd94c47d513
Manoj Paloi <paloimanoj@gmail.com>
2025-09-10 21:26:57 +0530

    add banners

--- drfcommerce/settings/base.py
+++ drfcommerce/settings/base.py
@@ -213,7 +213,6 @@ CORS_ALLOWED_ORIGINS = os.getenv(
     "http://localhost:3000,http://127.0.0.1:3000,https://mern-ecommerce-woad-three.vercel.app"
 ).split(",")
 
-
 CORS_ALLOW_METHODS = [
     "GET",
     "POST",
