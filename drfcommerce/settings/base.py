9388df67e24090128086c04c173758fb11fed07d
Manoj Paloi <paloimanoj@gmail.com>
2025-09-10 22:03:25 +0530

    add banners

--- drfcommerce/settings/base.py
+++ drfcommerce/settings/base.py
@@ -189,8 +189,8 @@ REST_FRAMEWORK = {
 # JWT Settings
 # -------------------------------------------------------------------
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),  
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),  
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
