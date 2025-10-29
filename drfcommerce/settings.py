102020ee020154e997b753d196eed8cb0e0e9b87
Manoj Paloi <paloimanoj@gmail.com>
2025-10-29 21:22:04 +0530

    Add Cloudinary for media storage

--- drfcommerce/settings.py
+++ drfcommerce/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------------------------------------------
 SECRET_KEY = os.getenv("SECRET_KEY", "changeme-in-production")
 DEBUG = os.getenv("DEBUG", "False").lower() in ["true", "1", "yes"]
-ALLOWED_HOSTS = ['13.49.70.126']
+ALLOWED_HOSTS = ['13.49.70.126', '*']
 
 # -------------------------------------------------------------------
 # Installed Apps
